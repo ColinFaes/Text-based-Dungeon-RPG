e9ba2265de997420ce92bf7bf7e189d470685a61
ColinFaes <colinfaes@gmail.com>
2024-09-12 11:09:04 +0200

    Generate random rooms + enemies
    Generate 5 random rooms met random enemies erin zodat je er doorheen kan en enemies tegen komt

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -10,6 +10,22 @@ namespace DungeonCrawler
     {
         static Random rand = new Random();
 
+        static List<Room> GenerateRandomDungeon()
+        {
+            List<Room> dungeon = new List<Room>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string roomDescription = GenerateRandomRoomDescription();
+                Enemy roomEnemy = GenerateRandomEnemy();
+                dungeon.Add(new Room(
+                    description: roomDescription,
+                    enemy: rand.Next(0, 2) == 0 ? roomEnemy : null,
+                ));
+            }
+            return dungeon;
+        }
+
         static string GenerateRandomRoomDescription()
         {
             string[] descriptions = {
@@ -42,4 +58,16 @@ namespace DungeonCrawler
                 defensePower: defensePower
             );
         }
+        static void Main(string[] args)
+        {
+            Player player;
+            List<Room> dungeon;
+            int currentRoomIndex = 0;
+                string playerName = Console.ReadLine();
+                player = new Player(playerName);
+                dungeon = GenerateRandomDungeon();
+            for (int i = currentRoomIndex; i < dungeon.Count; i++)
+            {
+                dungeon[i].Enter(player);
+            }
         }
