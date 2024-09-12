8d4c80cd16cdf90ac7eaf1c2eaf444e8d3a02b55
ColinFaes <colinfaes@gmail.com>
2024-09-12 11:03:10 +0200

    Random room description
    functie die een random description aan een room koppelt

---
+++ DungeonCrawler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DungeonCrawler
+{
+    class Program
+        static string GenerateRandomRoomDescription()
+        {
+            string[] descriptions = {
+                "A dark and damp cave.",
+                "A narrow corridor with flickering torches.",
+                "A grand hall with marble pillars.",
+                "A small room filled with cobwebs.",
+                "An ancient library with dusty tomes."
+            };
+        }
