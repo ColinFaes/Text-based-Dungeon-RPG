e77b3da6d62419f2ef9c6ce37a83cf3bf8d262fa
ColinFaes <colinfaes@gmail.com>
2024-09-12 10:42:27 +0200

    Room class aangemaakt

---
+++ DungeonCrawler/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int DefensePower { get; set; }
+        public string CurrentObjective { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Health = 100;
+            AttackPower = 10;
+            DefensePower = 5;
+            CurrentObjective = "Explore the dungeon, find the final bossroom and defeat the final boss.";
+        }
---
+++ DungeonCrawler/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DungeonCrawler
+{
+    public class Room
+    {
+        public string Description { get; set; }
+        public Enemy Enemy { get; set; }
+        public Room(string description, Enemy enemy = null, Item item = null, Item hiddenItem = null, Puzzle puzzle = null, NPC npc = null)
+        {
+            Description = description;
+            Enemy = enemy;
+    }
+}
