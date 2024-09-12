7a7143e35c5e9f2b63bb6b7afbc17c0f40506d55
ColinFaes <colinfaes@gmail.com>
2024-09-12 10:37:42 +0200

    Create Enemy.cs

---
+++ DungeonCrawler/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DungeonCrawler
+{
+    public class Enemy
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int DefensePower { get; set; }
+
+        public Enemy(string name, int health, int attackPower, int defensePower)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+            DefensePower = defensePower;
+        }
+    }
+}
