0b996de1662dbc7c27090e522cf9baee3dcb848d
ColinFaes <colinfaes@gmail.com>
2024-09-12 11:05:49 +0200

    Random enemy generator
    functie die een random enemy met random stats generate

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -7,6 +7,9 @@ using System.Xml.Serialization;
 namespace DungeonCrawler
 {
     class Program
+    {
+        static Random rand = new Random();
+
         static string GenerateRandomRoomDescription()
         {
             string[] descriptions = {
@@ -16,4 +19,27 @@ namespace DungeonCrawler
                 "A small room filled with cobwebs.",
                 "An ancient library with dusty tomes."
             };
+            return descriptions[rand.Next(descriptions.Length)];
+        }
+
+        static Enemy GenerateRandomEnemy()
+        {
+            string[] enemyNames = { 
+                "Goblin", 
+                "Skeleton", 
+                "Orc", 
+                "Zombie", 
+                "Dragon" 
+            };
+            int health = rand.Next(20, 101);
+            int attackPower = rand.Next(5, 21);
+            int defensePower = rand.Next(1, 11);
+
+            return new Enemy(
+                name: enemyNames[rand.Next(enemyNames.Length)],
+                health: health,
+                attackPower: attackPower,
+                defensePower: defensePower
+            );
+        }
         }
